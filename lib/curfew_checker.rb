cacf45e11d39e476ed254a9a6f7a80647306e214
Lillian Byrne <lily.byrne.304@gmail.com>
2018-06-24 11:41:27 +0000

    Done.

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -1,13 +1,28 @@
 def simple_curfew_checker(time)
-  # code goes here
+  if time > 11
+    puts "You're in trouble if it's than 11"
+  end 
 end
+
+
 
 def curfew_checker(time)
-  # code goes here
+  if time > 11
+    puts "You're in trouble if it's later than 11."
+  else 
+    puts ""
+  end 
 end
+curfew_checker(4)
 
 def complex_curfew_checker(time)
-  # code goes here
+  if time > 11 
+    puts "you're in trouble it it's 11"
+    elsif time == 11
+    puts "you're in trouble if it's "
+  else 
+    puts "Good luck buddy!"
+  end 
 end
 
 def deluxe_curfew_checker(time)
