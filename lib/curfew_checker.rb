5c0553eeb025d2fe3b78defe4e0c950dbf5e05ea
Lillian Byrne <lily.byrne.304@gmail.com>
2018-06-27 01:53:37 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -7,6 +7,8 @@ end
 def curfew_checker(time)
   if time == 11
     puts "You're in trouble! Better get home quick!"
+    elsif time >= 11 
+    puts "You're in trouble! Better get home quick!"
   end 
 end
 curfew_checker(4)
